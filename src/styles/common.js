c3cc0e2d7ac00e6b7fa3d336e8db45be33319fe7
Devin <devinyim1996@gmail.com>
2019-03-03 09:52:09 -0700

    Added more common styles

--- src/styles/common.js
+++ src/styles/common.js
@@ -2,5 +2,22 @@ export const COLOR = {
     primary: '#775FFB',
     black: '#000000',
     white: '#FFFFFF',
-    grey: '#888888'
+    grey: '#938E8E'
+};
+
+export const TEXT = {
+    input: {
+        color: '#888888',
+        fontSize: 16,
+    }
+};
+
+export const DEFAULT = {
+    shadow: {
+        elevation: 1,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 2,
+    }
 };

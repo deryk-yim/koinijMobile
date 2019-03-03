187b654e8d2fffecd16dcec27301ad1f0c82fb1b
Devin <devinyim1996@gmail.com>
2019-03-03 09:47:35 -0700

    Added to make it easy to access component

---
+++ src/component/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import Form from './Form';
+import Layout from './Layout';
+import Sidebar from './Sidebar';
+
+export default {
+    Button,
+    Form,
+    Layout,
+    Sidebar,
+};

532c414991d7c760758709063a3a4b208cce5063
Devin <devinyim1996@gmail.com>
2019-03-03 09:50:54 -0700

    Category constant added

---
+++ src/constant/category.js
@@ -0,0 +1,45 @@
+const CATEGORY = [
+    {
+        label: 'Category 1',
+        value: '1'
+    },
+    {
+        label: 'Category 2',
+        value: '2'
+    },
+    {
+        label: 'Category 3',
+        value: '3'
+    },
+    {
+        label: 'Category 4',
+        value: '4'
+    },
+    {
+        label: 'Category 5',
+        value: '5'
+    },
+    {
+        label: 'Category 6',
+        value: '6'
+    },
+    {
+        label: 'Category 7',
+        value: '7'
+    },
+    {
+        label: 'Category 8',
+        value: '8'
+    },
+    {
+        label: 'Category 9',
+        value: '9'
+    },
+    {
+        label: 'Category 10',
+        value: '10'
+    },
+];
+
+export default CATEGORY;
+
